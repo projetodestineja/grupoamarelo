4428c1737e6c381185ce3d48f5fdb71720f71f78
savifon <savifon1@gmail.com>
2017-09-14 19:18:19 -0300

    inserção de name nos inputs

--- destineja/application/views/empresas/coletor.php
+++ destineja/application/views/empresas/coletor.php
@@ -29,101 +29,101 @@ header('Content-Type: text/html; charset=utf-8');
 						<div class="form-row">
 						<div class="form-group col-md-4" id="col_cnpj">
 							<label for="cnpj" class="col-form-label">CNPJ</label>
-							<input required type="text" class="form-control cnpj" id="cnpj" placeholder="00.000.000/0000-00" onChange="valida_cnpj(form_cad_gerador.cnpj);">
+							<input required type="text" class="form-control cnpj" id="cnpj" name="cnpj" placeholder="00.000.000/0000-00" onChange="valida_cnpj(form_cad_gerador.cnpj);">
 						</div>
 						<div class="form-group col-md-4" id="col_rsocial">
 							<label required for="rsocial" class="col-form-label">Razão Social</label>
-							<input type="text" class="form-control" id="rsocial" placeholder="Razão Social">
+							<input type="text" class="form-control" id="rsocial" id="rsocial" placeholder="Razão Social">
 						</div>
 						<div class="form-group col-md-4" id="col_nfantasia">
 							<label for="nfantasia" class="col-form-label">Nome Fantasia</label>
-							<input type="text" class="form-control" id="nfantasia" placeholder="Nome Fantasia">
+							<input type="text" class="form-control" id="nfantasia" name="nfantasia" placeholder="Nome Fantasia">
 						</div>
 						</div>
 						<div class="form-row">
 						<div class="form-group col-md-8">
 							<label for="nresponsavel" class="col-form-label">Nome do Responsável</label>
-							<input required type="text" class="form-control" id="nresponsavel" placeholder="Ex.: César Silva, Amauri Jr...">
+							<input required type="text" class="form-control" id="nresponsavel" name="nresponsavel" placeholder="Ex.: César Silva, Amauri Jr...">
 						</div>
 						</div>
 						<h3 class="">Áreas de atuação</h3>
 						<div class="form-row">
 						<div class="col-sm-2 form-check">
-							<label class="form-check-label"><input type="checkbox" value=""> Option</label>
+							<label class="form-check-label"><input type="checkbox" name="checkbox1" value="opt1"> Option</label>
 						</div>
 						<div class="col-sm-2 form-check">
-							<label class="form-check-label"><input type="checkbox" value=""> Option</label>
+							<label class="form-check-label"><input type="checkbox" name="checkbox2" value=opt2""> Option</label>
 						</div>
 						<div class="col-sm-2 form-check">
-							<label class="form-check-label"><input type="checkbox" value=""> Option</label>
+							<label class="form-check-label"><input type="checkbox" name="checkbox3" value="opt3"> Option</label>
 						</div>
 						<div class="col-sm-2 form-check">
-							<label class="form-check-label"><input type="checkbox" value=""> Option</label>
+							<label class="form-check-label"><input type="checkbox" name="checkbox4"  value="opt4"> Option</label>
 						</div>
 						<div class="col-sm-2 form-check">
-							<label class="form-check-label"><input type="checkbox" value=""> Option</label>
+							<label class="form-check-label"><input type="checkbox" name="checkbox5" value="opt5"> Option</label>
 						</div>
 						<div class="col-sm-2 form-check">
-							<label class="form-check-label"><input type="checkbox" value=""> Option</label>
+							<label class="form-check-label"><input type="checkbox" name="checkbox6" value="opt6"> Option</label>
 						</div>
 						</div>
 						<h3 class="">Contato</h3>
 						<div class="form-row">
 						<div class="form-group col-md-4">
 							<label for="tel1" class="col-form-label">Telefone 1</label>
-							<input required type="text" class="form-control phone" id="tel1" placeholder="(21) 6564-0205, (27) 98500-6321...">
+							<input required type="text" class="form-control phone" id="tel1" name="tel1" placeholder="(21) 6564-0205, (27) 98500-6321...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="tel2" class="col-form-label">Telefone 2</label>
-							<input type="text" class="form-control phone" id="tel2" placeholder="(21) 6564-0205, (27) 98500-6321...">
+							<input type="text" class="form-control phone" id="tel2" name="tel2" placeholder="(21) 6564-0205, (27) 98500-6321...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="email" class="col-form-label">E-mail</label>
-							<input required type="email" class="form-control" id="email" placeholder="nome@dominio.com">
+							<input required type="email" class="form-control" id="email" name="email" placeholder="nome@dominio.com">
 						</div>
 						</div>
 						<h3 class="">Endereço</h3>
 						<div class="form-row">
 						<div class="form-group col-md-2">
 							<label for="cep" class="col-form-label">CEP</label>
-                                                        <input Vtype="text" class="form-control cep" id="cep" placeholder="000000-000" maxlength="8" onblur="pesquisacep(this.value);">
+              <input type="text" class="form-control cep" id="cep" name="cep" placeholder="000000-000" maxlength="8" onblur="pesquisacep(this.value);">
 						</div>
 						<div class="form-group col-md-5">
 							<label for="Rua" class="col-form-label">Rua</label>
-							<input required type="text" class="form-control" id="rua" placeholder="Ex.: Av. José Silva, Rua São Cosme...">
+							<input required type="text" class="form-control" id="rua" name="rua" placeholder="Ex.: Av. José Silva, Rua São Cosme...">
 						</div>
 						<div class="form-group col-md-2">
 							<label for="numero" class="col-form-label">Número</label>
-							<input required type="number" class="form-control" id="numero" placeholder="00">
+							<input required type="number" class="form-control" id="numero" name="numero" placeholder="00">
 						</div>
 						<div class="form-group col-md-3">
 							<label for="complemento" class="col-form-label">Complemento</label>
-							<input type="text" class="form-control" id="complemento" placeholder="Ex.: Casa, Apartamento...">
+							<input type="text" class="form-control" id="complemento" name="complemento" placeholder="Ex.: Casa, Apartamento...">
 						</div>
 						</div>
 						<div class="form-row">
 						<div class="form-group col-md-4">
 							<label for="bairro" class="col-form-label">Bairro</label>
-							<input type="text" class="form-control" id="bairro" placeholder="Ex.: São Gonçalo, Manguinhos...">
+							<input type="text" class="form-control" id="bairro" name="bairro" placeholder="Ex.: São Gonçalo, Manguinhos...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="cidade" class="col-form-label">Cidade</label>
-							<input type="text" class="form-control" id="cidade" placeholder="Ex.: Volta Redonda, Vila Velha...">
+							<input type="text" class="form-control" id="cidade" name="cidade" placeholder="Ex.: Volta Redonda, Vila Velha...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="estado" class="col-form-label">Estado</label>
-							<input type="text" class="form-control" id="estado" placeholder="Ex.: Rio de Janeiro, Espírito Santo...">
+							<input type="text" class="form-control" id="estado" name="estado" placeholder="Ex.: Rio de Janeiro, Espírito Santo...">
 						</div>
 						</div>
 						<h3 class="">Acesso</h3>
 						<div class="form-row">
 						<div class="form-group col-md-6">
 							<label for="senha1" class="col-form-label">Digite sua Senha</label>
-							<input required type="password" class="form-control" id="senha1" placeholder="Digite sua Senha">
+							<input required type="password" class="form-control" id="senha1" name="senha1" placeholder="Digite sua Senha">
 						</div>
 						<div class="form-group col-md-6">
 							<label for="senha2" class="col-form-label">Confirme sua Senha</label>
-							<input required type="password" class="form-control" id="senha2" onchange="valida_senha();" placeholder="Confirme sua Senha">
+							<input required type="password" class="form-control" id="senha2" name="senha2" onchange="valida_senha();" placeholder="Confirme sua Senha">
 						</div>
 						</div>
 						<a href="<?php echo base_url('') ?>"><button class="btn btn-outline-secondary" type="button">Voltar</button></a>
@@ -140,6 +140,6 @@ header('Content-Type: text/html; charset=utf-8');
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
 	<script src="<?php echo base_url('assets/js/jquery.mask.js') ?>"></script>
 	<script src="<?php echo base_url('assets/js/js.js') ?>"></script>
-        <script src="<?php echo site_url('assets/js/buscacep.js') ?>"></script>
+  <script src="<?php echo site_url('assets/js/buscacep.js') ?>"></script>
 
 </html>
--- destineja/application/views/empresas/gerador.php
+++ destineja/application/views/empresas/gerador.php
@@ -5,7 +5,7 @@ header('Content-Type: text/html; charset=utf-8');
 <!DOCTYPE html>
 
 <html lang="pt-br">
-	
+
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -18,23 +18,23 @@ header('Content-Type: text/html; charset=utf-8');
 	</head>
 
 	<body>
-		
+
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
 					<img width="200px" src="<?php echo base_url('assets/img/destinejalogo.png') ?>"/><br/>
 					<h1 class="h1Forms">Cadastro - Gerador de Resíduo</h1>
-                                        <form id="form_cad_gerador" action="<?php echo site_url('empresa/cadastrar'); ?>" method="POST">
+     			<form id="form_cad_gerador" action="<?php echo site_url('empresa/cadastrar'); ?>" method="POST">
 						<h3 class="">Tipo de cadastro</h3>
 						<div class="form-row">
 						<div class="form-check form-check-inline">
 							<label class="form-check-label">
-							<input  checked class="form-check-input" type="radio" name="tipo_cadastro" id="pjuridica" value="pjuridica"> Pessoa Jurídica</input>
+							<input checked class="form-check-input" type="radio" name="tipo_cadastro" id="pjuridica" name="pjuridica" value="pjuridica"> Pessoa Jurídica</input>
 							</label>
 						</div>
 						<div class="form-check form-check-inline">
 							<label class="form-check-label">
-							<input  class="form-check-input" type="radio" name="tipo_cadastro" id="pfisica" value="pfisica"> Pessoa Física</input>
+							<input class="form-check-input" type="radio" name="tipo_cadastro" id="pfisica" name="pfisica" value="pfisica"> Pessoa Física</input>
 							</label>
 						</div>
 						</div>
@@ -42,29 +42,29 @@ header('Content-Type: text/html; charset=utf-8');
 						<div class="form-row">
 						<div class="form-group col-md-4" id="col_cnpj">
 							<label for="cnpj" class="col-form-label">CNPJ</label>
-							<input  type="text" class="form-control cnpj" id="cnpj" placeholder="00.000.000/0000-00" onChange="valida_cnpj(form_cad_gerador.cnpj);">
+							<input type="text" class="form-control cnpj" id="cnpj" name="cnpj" placeholder="00.000.000/0000-00" onChange="valida_cnpj(form_cad_gerador.cnpj);">
 						</div>
 						<div class="form-group col-md-4" id="col_cpf">
 							<label for="cpf" class="col-form-label">CPF</label>
-							<input  type="text" class="form-control cpf" id="cpf" placeholder="000.000.000-00" onChange="valida_cpf(form_cad_gerador.cpf);">
+							<input type="text" class="form-control cpf" id="cpf" name="cpf" placeholder="000.000.000-00" onChange="valida_cpf(form_cad_gerador.cpf);">
 						</div>
 						<div class="form-group col-md-4" id="col_rsocial">
-							<label  for="rsocial" class="col-form-label">Razão Social</label>
-							<input type="text" class="form-control" id="rsocial" placeholder="Razão Social">
+							<label for="rsocial" class="col-form-label">Razão Social</label>
+							<input type="text" class="form-control" id="rsocial" name="rsocial" placeholder="Razão Social">
 						</div>
 						<div class="form-group col-md-4" id="col_nfantasia">
 							<label for="nfantasia" class="col-form-label">Nome Fantasia</label>
-							<input type="text" class="form-control" id="nfantasia" placeholder="Nome Fantasia">
+							<input type="text" class="form-control" id="nfantasia" name="nfantasia" placeholder="Nome Fantasia">
 						</div>
 						</div>
 						<div class="form-row">
 						<div class="form-group col-md-4">
 							<label for="nresponsavel" class="col-form-label">Nome do Responsável</label>
-							<input  type="text" class="form-control" id="nresponsavel" placeholder="Ex.: César Silva, Amauri Jr...">
+							<input type="text" class="form-control" id="nresponsavel" name="nresponsavel" placeholder="Ex.: César Silva, Amauri Jr...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="rnegocio">Ramo de Negócio</label>
-							<select  class="form-control" id="rnegocio">
+							<select class="form-control" id="rnegocio">
 								<option selected value=""></option>
 								<option value="">Oficina Mecânica</option>
 								<option value="">Posto de Gasolina</option>
@@ -75,66 +75,66 @@ header('Content-Type: text/html; charset=utf-8');
 						</div>
 						<div class="form-group col-md-4" id="outro_ramo_option">
 							<label for="digite_ramo" class="col-form-label">Digite o Ramo de Negócio</label>
-							<input  type="text" class="form-control" id="digite_ramo" placeholder="Especifique o Ramo de Negócio">
+							<input type="text" class="form-control" id="digite_ramo" name="digite_ramo" placeholder="Especifique o Ramo de Negócio">
 						</div>
 						</div>
 						<h3 class="">Contato</h3>
 						<div class="form-row">
 						<div class="form-group col-md-4">
 							<label for="tel1" class="col-form-label">Telefone 1</label>
-							<input  type="text" class="form-control phone" id="tel1" placeholder="(21) 6564-0205, (27) 98500-6321...">
+							<input type="text" class="form-control phone" id="tel1" name="tel1" placeholder="(21) 6564-0205, (27) 98500-6321...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="tel2" class="col-form-label">Telefone 2</label>
-							<input type="text" class="form-control phone" id="tel2" placeholder="(21) 6564-0205, (27) 98500-6321...">
+							<input type="text" class="form-control phone" id="tel2" name="tel2" placeholder="(21) 6564-0205, (27) 98500-6321...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="email" class="col-form-label">E-mail</label>
-                                                        <input  type="email" class="form-control" id="email" placeholder="nome@dominio.com">
+							<input type="email" class="form-control" id="email" name="email" placeholder="nome@dominio.com">
 						</div>
 						</div>
 						<h3 class="">Endereço</h3>
 						<div class="form-row">
 						<div class="form-group col-md-2">
 							<label for="cep" class="col-form-label">CEP</label>
-                                                        <input type="text" class="form-control cep" id="cep" placeholder="000000-000" maxlength="8" onblur="pesquisacep(this.value);">
+							<input type="text" class="form-control cep" id="cep" name="cep" placeholder="000000-000" maxlength="8" onblur="pesquisacep(this.value);">
 						</div>
 						<div class="form-group col-md-5">
 							<label for="Rua" class="col-form-label">Rua</label>
-							<input  type="text" class="form-control" id="rua" placeholder="Ex.: Av. José Silva, Rua São Cosme...">
+							<input type="text" class="form-control" id="rua" name="rua" placeholder="Ex.: Av. José Silva, Rua São Cosme...">
 						</div>
 						<div class="form-group col-md-2">
 							<label for="numero" class="col-form-label">Número</label>
-							<input  type="number" class="form-control" id="numero" placeholder="00">
+							<input type="number" class="form-control" id="numero" name="numero" placeholder="00">
 						</div>
 						<div class="form-group col-md-3">
 							<label for="complemento" class="col-form-label">Complemento</label>
-							<input type="text" class="form-control" id="complemento" placeholder="Ex.: Casa, Apartamento,...">
+							<input type="text" class="form-control" id="complemento" name="complemento" placeholder="Ex.: Casa, Apartamento,...">
 						</div>
 						</div>
 						<div class="form-row">
 						<div class="form-group col-md-4">
 							<label for="bairro" class="col-form-label">Bairro</label>
-							<input type="text" class="form-control" id="bairro" placeholder="Ex.: São Gonçalo, Manguinhos...">
+							<input type="text" class="form-control" id="bairro" name="bairro" placeholder="Ex.: São Gonçalo, Manguinhos...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="cidade" class="col-form-label">Cidade</label>
-							<input type="text" class="form-control" id="cidade" placeholder="Ex.: Volta Redonda, Vila Velha...">
+							<input type="text" class="form-control" id="cidade" name="cidade" placeholder="Ex.: Volta Redonda, Vila Velha...">
 						</div>
 						<div class="form-group col-md-4">
 							<label for="estado" class="col-form-label">Estado</label>
-							<input type="text" class="form-control" id="estado" placeholder="Ex.: Rio de Janeiro, Espírito Santo...">
+							<input type="text" class="form-control" id="estado" name="estado" placeholder="Ex.: Rio de Janeiro, Espírito Santo...">
 						</div>
 						</div>
 						<h3 class="">Acesso</h3>
 						<div class="form-row">
 						<div class="form-group col-md-6">
 							<label for="senha1" class="col-form-label">Digite sua Senha</label>
-							<input  type="password" class="form-control" id="senha1" placeholder="Digite sua Senha">
+							<input type="password" class="form-control" id="senha1" name="senha1" placeholder="Digite sua Senha">
 						</div>
 						<div class="form-group col-md-6">
 							<label for="senha2" class="col-form-label">Confirme sua Senha</label>
-							<input  type="password" class="form-control" id="senha2" onchange="valida_senha();" placeholder="Confirme sua Senha">
+							<input type="password" class="form-control" id="senha2" name="senha2" onchange="valida_senha();" placeholder="Confirme sua Senha">
 						</div>
 						</div>
 						<a href="<?php echo base_url('') ?>"><button class="btn btn-outline-secondary" type="button">Voltar</button></a>
@@ -143,15 +143,15 @@ header('Content-Type: text/html; charset=utf-8');
 				</div>
 			</div>
 		</div>
-	
+
 	</body>
-	
+
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
 	<script src="<?php echo site_url('assets/js/jquery.mask.js') ?>"></script>
 	<script src="<?php echo site_url('assets/js/js.js') ?>"></script>
-        <script src="<?php echo site_url('assets/js/buscacep.js') ?>"></script>
+	<script src="<?php echo site_url('assets/js/buscacep.js') ?>"></script>
 	<script type="text/javascript">
 	$( "#pjuridica" ).click(function() {
 		$( "#col_cnpj" ).show();
