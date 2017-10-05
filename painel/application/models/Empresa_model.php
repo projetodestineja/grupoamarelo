0480543d3636b4cc0cac28e08ec79e4bd46939a7
onerio <onerio@192.168.25.2>
2017-10-04 23:23:28 -0300

    Model empresa ajustado para receber array com o atuação da empresa

--- painel/application/models/Empresa_model.php
+++ painel/application/models/Empresa_model.php
@@ -1,61 +1,86 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Empresa_model extends CI_Model {
-	
-	public function get_result_all($funcoes_empresa){
-	  if($funcoes_empresa){
-		$this->db->where('id_funcao',(int)$funcoes_empresa);  
-	  }	
-      return $this->db->get('empresas')->result();
-    }
-	
-	public function get_funcao_row($id){
-	  $this->db->where('id',(int)$id);  
-	  return $this->db->get('funcoes_empresas')->row();
-    }
-	
-    public function gravar($dados){
-      $this->db->insert('empresas',$dados);
+
+    public function get_result_all($funcoes_empresa) {
+        if ($funcoes_empresa) {
+            $this->db->where('id_funcao', (int) $funcoes_empresa);
+        }
+        return $this->db->get('empresas')->result();
     }
 
-    public function desbloquear($acao,$id){
-      $this->db->query("UPDATE empresas SET ativo = $acao WHERE empresas.id = ".(int)$id."");
+    public function get_funcao_row($id) {
+        $this->db->where('id', (int) $id);
+        return $this->db->get('funcoes_empresas')->row();
     }
 
-	
-    public function atualizar($dados){
-      $this->db->where('id',$dados['id']);
-      $this->db->update('empresas',$dados);
+    public function gravar($dados) {
+        $this->db->insert('empresas', $dados);
     }
 
-    public function consultar($tipo){
-      $this->db->where('id_funcao',$tipo);
-      $this->db->order_by("id", "desc");
-      return $data['result'] = $this->db->get('empresas')->result();
+    public function desbloquear($acao, $id) {
+        $this->db->query("UPDATE empresas SET ativo = $acao WHERE empresas.id = " . (int) $id . "");
     }
 
-    public function consultar_coletoraId($id){
-      $this->db->where('id',(int)$id);
-      return $this->db->get('empresas')->row();
+    public function atualizar($dados) {
+        $this->db->where('id', $dados['id']);
+        $this->db->update('empresas', $dados);
     }
 
-    public function consultar_area_Id($id){
-      $this->db->where('id_empresa',$id);
-      $this->db->where('principal',1);
-      return $this->db->get('empresas_areas_atuacao')->row();
+    public function consultar($tipo) {
+        $this->db->where('id_funcao', $tipo);
+        $this->db->order_by("id", "desc");
+        return $data['result'] = $this->db->get('empresas')->result();
     }
 
-    public function consultar_area_secundaria_Id($id){
-      return $data['result3'] = $this->db->query("SELECT * FROM areas_atuacao JOIN (empresas_areas_atuacao) ON (empresas_areas_atuacao.id_empresa = ".(int)$id." AND areas_atuacao.codigo = empresas_areas_atuacao.codigo_area_atuacao AND empresas_areas_atuacao.principal=2) ")->result();
+    public function consultar_coletoraId($id) {
+        $this->db->where('id', (int) $id);
+        return $this->db->get('empresas')->row();
     }
 
-    public function get_all_area_atuacao(){
-        $this->db->order_by('area_atuacao','asc');
-		//$this->db->limit(50);
+    public function consultar_area_Id($id) {
+        $this->db->where('id_empresa', $id);
+        $this->db->where('principal', 1);
+        return $this->db->get('empresas_areas_atuacao')->row();
+    }
+
+    public function consultar_area_secundaria_Id($id) {
+        return $data['result3'] = $this->db->query("SELECT * FROM areas_atuacao JOIN (empresas_areas_atuacao) ON (empresas_areas_atuacao.id_empresa = " . (int) $id . " AND areas_atuacao.codigo = empresas_areas_atuacao.codigo_area_atuacao AND empresas_areas_atuacao.principal=2) ")->result();
+    }
+
+    public function get_all_area_atuacao() {
+        $this->db->order_by('area_atuacao', 'asc');
+        //$this->db->limit(50);
         return $this->db->get('areas_atuacao')->result();
     }
-	
-	
+
+    function atuacao($id_empresa) {
+        // Limpamos tudo
+        $this->db->where('id_empresa', (int) $id_empresa);
+        $this->db->delete('empresas_areas_atuacao');
+
+        //Montamos a primeira array com a atividade principal
+        $data[] = array(
+            'principal' => 1,
+            'id_empresa' => (int) $id_empresa,
+            'codigo_area_atuacao' => $this->input->post('area_atuacao'),
+            'outra_area_atuacao' => $this->input->post('digite_area')
+        );
+
+        //Montamos as demais arrays com as secundarias
+        foreach ($this->input->post('atuacao_secundaria') as $codigo) {
+            $data[] = array(
+                'principal' => 2,
+                'id_empresa' => (int) $id_empresa,
+                'codigo_area_atuacao' => $codigo,
+                'outra_area_atuacao' => NULL
+            );
+        }
+
+        //Fazemos o insert pegando a array montada acima
+        $this->db->insert_batch('empresas_areas_atuacao', $data);
+    }
 
 }
