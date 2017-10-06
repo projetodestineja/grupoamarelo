06e8e191db1a3e52e5ffae1b73a03693456ab802
savifon <savifon1@gmail.com>
2017-10-05 21:52:49 -0300

    telas do painel padronizadas (cores, logo destinejá e fontes); cadastro e edição de áreas de atuação;

--- painel/application/views/theme/default.php
+++ painel/application/views/theme/default.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo $title; ?></title>
-    
+
     	<?php
 		/** -- Copy from here -- */
 		if(!empty($meta))
@@ -16,12 +16,12 @@
 			?><meta name="<?php echo $name; ?>" content="<?php echo $content; ?>" /><?php
 				 }
 		echo "\n";
-	
+
 		if(!empty($canonical))
 		{
 			echo "\n\t\t";
 			?><link rel="canonical" href="<?php echo $canonical?>" /><?php
-	
+
 		}
 		echo "\n\t";
 		/** -- to here -- */
@@ -41,44 +41,45 @@
 
   </head>
 
-  <body class="fixed-nav sticky-footer bg-dark <?php echo ((isset($_COOKIE['menu-vertital-painel']) and $_COOKIE['menu-vertital-painel']=='sim')?'sidenav-toggled':''); ?>" id="page-top">
+  <body class="fixed-nav sticky-footer bg-light <?php echo ((isset($_COOKIE['menu-vertital-painel']) and $_COOKIE['menu-vertital-painel']=='sim')?'sidenav-toggled':''); ?>" id="page-top">
 
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
       <a class="navbar-brand" href="<?php echo site_url('painel'); ?>" >
-            <i class="fa fa-fw fa-lock"></i> <?php echo $this->config->item('title'); ?>
+            <!--i class="fa fa-fw fa-lock"></i> <php echo $this->config->item('title'); ?-->
+            <img style="height:25px;" src="<?php echo base_url('assets/img/destinejalogo.png'); ?>"/>
       </a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">         
+      <div class="collapse navbar-collapse" id="navbarResponsive">
         <?php $this->load->view('theme/menu');?>
       </div>
     </nav>
-   
-   
-   
-   
-    
+
+
+
+
+
     <div class="content-wrapper">
 
       <div class="container-fluid">
-		<?php if(isset($menu_mapa)){ ?>	
+		<?php if(isset($menu_mapa)){ ?>
         <!-- Breadcrumbs -->
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
             <a href="<?php echo site_url(); ?>">Painel</a>
           </li>
-          <?php 
-          
-            foreach($menu_mapa as $key => $item) { 
-              if(empty($item)){  
+          <?php
+
+            foreach($menu_mapa as $key => $item) {
+              if(empty($item)){
                 echo '<li class="breadcrumb-item active" >'.$key.'</li>';
               }else{
-                echo '<li class="breadcrumb-item" ><a href="'.site_url($item).'" >'.$key.'</a></li>';  
+                echo '<li class="breadcrumb-item" ><a href="'.site_url($item).'" >'.$key.'</a></li>';
               }
             }
-          
+
           ?>
         </ol>
         <?php } ?>
@@ -86,23 +87,23 @@
             <div class="col-md-4">
                 <h1 style="font-size:27px;"><?php echo $title; ?></h1>
             </div>
-            <div class="col-md-8 text-right">  
+            <div class="col-md-8 text-right">
                 <span id="colvis"></span>
-                <?php 
+                <?php
 		if(isset($menu_opcao_direita)){
                     foreach($menu_opcao_direita as $menu_r){
                         echo ' '.$menu_r;
-                    } 
-		} 
+                    }
+		}
 		?>
             </div>
         </div>
-        
-        <?php 
+
+        <?php
 	if($this->session->flashdata('resposta_erro') or isset($resposta_erro)){ ?>
-            <div class="alert alert-danger" > 
+            <div class="alert alert-danger" >
              <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
-            
+
             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 			<?php echo ($this->session->flashdata('resposta_erro')?$this->session->flashdata('resposta_erro'):''); ?>
             <?php echo ($resposta_erro?$resposta_erro:''); ?>
@@ -113,20 +114,20 @@
            <i class="fa fa-check-circle-o" aria-hidden="true"></i>
             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 		<?php echo $this->session->flashdata('resposta_ok'); ?>
-                
+
             </div>
         <?php } ?>
-        
+
         <div class="conteudo">
             <?php echo $output;?>
         </div>
-        
+
      </div>
     </div>
-    
-    
-    
-    
+
+
+
+
     <footer class="sticky-footer">
       <div class="container">
         <div class="text-center">
@@ -140,19 +141,19 @@
       <i class="fa fa-angle-up"></i>
     </a>
 
-	
+
     <!-- Modal sair do painel-->
     <?php $this->load->view('modal/sair'); ?>
-    
+
 
     <!-- Bootstrap core JavaScript -->
     <script src="<?php echo base_url('assets/pluguins/jquery/jquery.min.js'); ?>"></script>
     <script src="<?php echo base_url('assets/pluguins/popper/popper.min.js'); ?>"></script>
     <script src="<?php echo base_url('assets/pluguins/bootstrap/js/bootstrap.min.js'); ?>"></script>
- 
+
     <!-- Plugin JavaScript -->
     <script src="<?php echo base_url('assets/pluguins/jquery-easing/jquery.easing.min.js'); ?>"></script>
- 
+
     <?php
       // Load CSS Controller
       foreach($css as $file){
@@ -160,34 +161,34 @@
     ?>
         <link rel="stylesheet" href="<?php echo $file; ?>" type="text/css" />
     <?php
-    } 
+    }
     echo "\n\t";
-	
+
     // Load JS Controller
     foreach($js as $file){
     echo "\n\t\t";
     ?>
 	<script src="<?php echo $file; ?>"></script>
     <?php
-    } 
+    }
     echo "\n\t";
     /*
     <script src="http://livedemo.mbahcoding.com/assets/datatables.colvis/dataTables.colVis.js"></script>
     <link href="http://livedemo.mbahcoding.com/assets/datatables.colvis/dataTables.colVis.css" rel="stylesheet">
      */
     ?>
-    <script src="<?php echo site_url('assets/pluguins/buscacep.js') ?>"></script>           
+    <script src="<?php echo site_url('assets/pluguins/buscacep.js') ?>"></script>
     <script src="<?php echo site_url('assets/pluguins/jquery.mask.js') ?>"></script>
 	<script src="<?php echo site_url('assets/js/js.js') ?>"></script>
-   
-    
+
+
     <!-- Custom scripts for this template -->
     <script src="<?php echo base_url('assets/js/sb-admin.js'); ?>"></script>
-    
+
     <script>
-	
+
 	function form_empresa(value){
-		
+
 		if(value=='F'){
 			$('.col-pjuridica').hide();
 			$('.col-pfisica').show();
@@ -198,11 +199,11 @@
 			$('.col-pjuridica').show();
 			document.getElementById("cpf").required = false;
 			document.getElementById("cnpj").required = true;
-		}	
+		}
 	}
-	
+
 	function load_cidades(estado,cidade=NUll){
-		   	
+
            $.getJSON( '<?php echo site_url(); ?>' + 'endereco/getcidades/' + estado+'?cidade='+cidade, function (data){
 		      var option = new Array();
 			     $.each(data, function(i, obj){
@@ -213,30 +214,30 @@
                     }
                    $(option[i]).append( obj.nome_cidade );
                     $("select[name=cidade]").append( option[i] );
-                 }); 
-           });  
-       
+                 });
+           });
+
     }
 
     function resetaCombo( el ) {
            $("select[name='"+el+"']").empty();
-           var option = document.createElement('option');                                  
+           var option = document.createElement('option');
            $(option).attr( {value : ''} );
            $(option).append( 'Selecione a Cidade' );
            $("select[name='"+el+"']").append( option );
     }
-	
+
 	$(document).ready(function(){
-		
+
 			if($('input[name=tipo_cadastro]:checked').val()){
 				form_empresa($('input[name=tipo_cadastro]:checked').val());
 			}
-			
+
 			$("input[name=tipo_cadastro]" ).click(function() {
 				form_empresa($(this).val());
 			});
-			
-		
+
+
 			$( "#rnegocio" ).change(function() {
 				if (this.value == "outro"){
 					$( "#outro_ramo_option" ).show();
@@ -245,28 +246,28 @@
 					$( "#outro_ramo_option" ).hide();
 				}
 			});
-	
-			
-			$('[data-toggle="tooltip"]').tooltip(); 
+
+
+			$('[data-toggle="tooltip"]').tooltip();
 			$('.menu-vertical-principal .menu-v-<?php echo $this->uri->segment(1); ?>').addClass("active");
-			
+
 			$("select[name=estado]").change(function(){
 	           var estado = $(this).val();
 			      resetaCombo('cidade');
                 load_cidades(estado,'');
             });
-			
-			
+
+
 		});
-	    
-       
+
+
     </script>
-    
-	<?php 
+
+	<?php
 	//Carregamento javascript datatables JS
 	if(isset($datagrid_js)){echo $datagrid_js;}
 	?>
-  
+
 
   </body>
 
