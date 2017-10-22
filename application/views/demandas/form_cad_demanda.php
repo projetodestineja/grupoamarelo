b9f0c58c176f758d3f3086a1e293cf9afd4156c2
savifon <savifon1@gmail.com>
2017-10-22 12:28:56 -0200

    correção de formulário de cadastro de demandas

--- application/views/demandas/form_cad_demanda.php
+++ application/views/demandas/form_cad_demanda.php
@@ -1,19 +1,42 @@
-<form id="form_cad_area_atuacao" action="" method="POST">
+<form id="form_cad_demanda" action="" method="POST">
 
 		<div class="form-row">
-			<div class="form-group col-md-12  required">
-				<label for="area_atuacao" class="col-form-label">Descrição da Área de Atuação:</label>
-				<input type="text" class="form-control" id="area_atuacao" value="<?php echo $area_atuacao; ?>" name="area_atuacao" placeholder="Digite a descrição da área de atuação">
+			<div class="form-group col-md-3 required">
+				<label for="data_inicio" class="col-form-label">Data de Início do Anúncio:</label>
+				<input type="date" class="form-control date" id="data_inicio" value="<?php echo $data_inicio; ?>" name="data_inicio" placeholder="Escolha uma data para o anúncio ser disponibilizado">
 			</div>
-      <div class="form-group col-md-4  required">
-				<label for="codigo" class="col-form-label">Código</label>
-				<input type="text" class="form-control" id="codigo" value="<?php echo $codigo; ?>" name="codigo" placeholder="Digite o código da área de atuação">
+      <div class="form-group col-md-3 required">
+				<label for="data_validade" class="col-form-label">Data de Expiração do Anúncio:</label>
+				<input type="date" class="form-control date" id="data_validade" value="<?php echo $data_validade; ?>" name="data_validade" placeholder="Escolha uma data para o anúncio expirar">
 			</div>
 		</div>
-    <div class="form-row">
-      <div class="form-group col-md-4">
-        <label for="ativo" class="col-form-label">Área de Atuação ativa?</label>
-        <input name="ativo" type="checkbox" value="1" <?php echo (($ativo==1 or !isset($id))?'checked':''); ?> >
+    
+		<div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="residuo" class="col-form-label">Especifique o resíduo:</label>
+				<input type="text" class="form-control" id="residuo" value="<?php echo $residuo; ?>" name="residuo" placeholder="Ex.: Oléo de cozinha usado">
+      </div>
+			<div class="form-group col-md-6">
+        <label for="condicionado" class="col-form-label">Como o resíduo está acondicionado?</label>
+				<input type="text" class="form-control" id="condicionado" value="<?php echo $condicionado; ?>" name="condicionado" placeholder="Ex.: Garrafas pet 2 litros">
+      </div>
+    </div>
+
+		<div class="form-row">
+      <div class="form-group col-md-3">
+        <label for="qtd" class="col-form-label">Quantidade:</label>
+				<input type="number" class="form-control" id="qtd" value="<?php echo $qtd; ?>" name="qtd" placeholder="Ex.: 11,5">
+      </div>
+			<div class="form-group col-md-3">
+        <label for="uni_medida" class="col-form-label">Unidade de Medida:</label>
+				<input type="text" class="form-control" id="uni_medida" value="<?php echo $uni_medida; ?>" name="uni_medida" placeholder="Ex.: Garrafas pet 2 litros">
+      </div>
+    </div>
+
+		<div class="form-row">
+      <div class="form-group col-md-12">
+        <label for="obs" class="col-form-label">Observações:</label>
+				<textarea class="form-control" id="obs" value="<?php echo $obs; ?>" name="obs" rows="5" placeholder="Utilize este campo para observações tais como: dificuldade para entrada de veículos grandes, disponibilidade de horário, etc."></textarea>
       </div>
     </div>
 
