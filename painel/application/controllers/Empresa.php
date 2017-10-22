66821b7d63f455d461c4f3ec0544e8b282ca5e21
Anderson <Anderson@192.168.2.104>
2017-10-22 21:42:48 -0200

    Aba de categorias de resíduos coletados no PAINEL DESTINE JÁ. Update OK.

--- painel/application/controllers/Empresa.php
+++ painel/application/controllers/Empresa.php
@@ -194,6 +194,10 @@ class Empresa extends CI_Controller {
         $data['result_atuacoes'] = $this->empresa_model->consultar_area_secundaria_Id($id);
         $data['areas_atuacoes'] = $this->empresa_model->get_all_area_atuacao();
 
+        //Buscando categorias de residuos coletados
+        $data['categorias_residuos'] = $this->empresa_model->get_all_categorias_residuos($data['id']);
+        
+        
         //Trabalho o select no form
         $uf = ($this->input->post('estado') ? $this->input->post('estado') : $row->uf_estado);
         $data['estados'] = $this->endereco_model->get_all_estados(); // Listamos todos estados normalmente
@@ -279,7 +283,7 @@ class Empresa extends CI_Controller {
 				$this->empresa_model->empresa_update($id, $data, $post);
 
                 $this->empresa_model->atuacao((int) $id); //Atualizar o tabela atuacao
-
+                $this->empresa_model->update_categorias_residuos((int) $id);
                 $resposta = 'Empresa coletora atualizada com sucesso.';
             }
             $retorno = true;
