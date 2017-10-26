a383a97e2ef4a01ba0425ac3354827386305b7ea
Anderson Tadeu Sgrancio <tadeusgrancio@gmail.com>
2017-10-26 12:34:13 -0200

    Lista demandas responsivo

--- application/views/demanda/lista_demandas.php
+++ application/views/demanda/lista_demandas.php
@@ -4,39 +4,104 @@
     <div class="row" style="margin:20px;">
         <thead>
             <tr>
-                <th width="110"><center>Imagem</center></th>
-                <th><center>Descrição</center></th>
-                <th><center>Período</center></th>
-                <th width="70"><center>Status</center></th>
+                <th ><center>Demanda</center></th>
+                <th ><center>Demanda</center></th>
+                <th ><center>Demanda</center></th>
             </tr>
         </thead>
     </div>
     <tbody>
         
         <?php
-        foreach ($demandas as $key){
+        $i = 0;
+        
+        for ($i ; $i < count($demandas) ; $i++) {   
+          
+        if (isset($demandas[$i])) $key = $demandas[$i]; else $key = Null;    
+    
         ?>
+        
         <tr>
             <td>
+                <?php if ($key){ ?>
                 <div style="padding:5px;">
-                    <img src="<?php echo $key->img; ?>" class="img-rounded" height="100" width="100">
+                    <div style="text-align: center">
+                        <div>
+                            <img src="<?php if (isset($key->img)) echo $key->img; else echo "http://www.premiermax.com.br/wp-content/uploads/2015/12/Sem-Imagem.jpg" ; ?>" class="img-rounded" height="100" width="100">
+                        </div>
+                        <div style="padding:10px;">
+                            <?php echo "<b>".$key->residuo."</b>"; ?>
+                            <?php echo "<br>".$key->nome_cidade.", ".$key->col_uf_estado."<br>"; ?>
+                            <button type="button" class="btn btn-secondary btn-sm" data-toggle="collapse" data-target="#descricao<?php echo $i;?>">Mais Informações</button>    
+                        </div>
+                    </div>    
+                    <div id="descricao<?php echo $i;?>" class="collapse">
+                        <?php echo "Quantidade: ".$key->qtd.$key->uni_medida; ?><br>
+                        <div style="text-align: justify;">
+                            <?php echo $key->obs; ?>
+                        </div>
+                        <?php echo  "Período: ".date('d/m/Y',strtotime($key->data_inicio)). " à ".date('d/m/Y',strtotime($key->data_validade)); ?>
+                        <div style="color:<?php echo $key->cor; ?>;"> 
+                            <?php echo "*".$key->descricao; ?>
+                        </div>
+                        <center><button class="btn btn-primary btn-block" value="Ver" > Ver</button></center>
+                    </div>
                 </div>
+                <?php $i++; if (isset($demandas[$i])) $key = $demandas[$i]; else $key = Null;   }?>
             </td>
             <td>
-                <div>
-                    <b><?php echo $key->residuo; ?></b>
+                <?php if ($key){ ?>
+                  <div style="padding:5px;">
+                    <div style="text-align: center">
+                        <div>
+                            <img src="<?php if (isset($key->img)) echo $key->img; else echo "http://www.premiermax.com.br/wp-content/uploads/2015/12/Sem-Imagem.jpg" ; ?>" class="img-rounded" height="100" width="100">
+                        </div>
+                        <div style="padding:10px;">
+                            <?php echo "<b>".$key->residuo."</b>"; ?>
+                            <?php echo "<br>".$key->nome_cidade.", ".$key->col_uf_estado."<br>"; ?>
+                            <button type="button" class="btn btn-secondary btn-sm" data-toggle="collapse" data-target="#descricao<?php echo $i;?>">Mais Informações</button>    
+                        </div>
+                    </div>    
+                    <div id="descricao<?php echo $i;?>" class="collapse">
+                        <?php echo "Quantidade: ".$key->qtd.$key->uni_medida; ?><br>
+                        <div style="text-align: justify;">
+                            <?php echo $key->obs; ?>
+                        </div>
+                        <?php echo  "Período: ".date('d/m/Y',strtotime($key->data_inicio)). " à ".date('d/m/Y',strtotime($key->data_validade)); ?>
+                        <div style="color:<?php echo $key->cor; ?>;"> 
+                            <?php echo "*".$key->descricao; ?>
+                        </div>
+                   <center><button class="btn btn-primary btn-block " value="Ver" > Ver</button></center>
+                    </div>
                 </div>
-                <div>
-                    <?php echo "Quantidade: ".$key->qtd.$key->uni_medida."<br>".$key->obs; ?><br>
-                    <?php echo $key->nome_cidade.", ".$key->col_uf_estado; ?>
+                <?php $i++; if (isset($demandas[$i])) $key = $demandas[$i]; else $key = Null;  }?>
+            </td>           
+            <td>
+                <?php if ($key){ ?>
+                <div style="padding:5px;">
+                    <div style="text-align: center">
+                        <div style="text-align: center">
+                            <img src="<?php if (isset($key->img)) echo $key->img; else echo "http://www.premiermax.com.br/wp-content/uploads/2015/12/Sem-Imagem.jpg" ; ?>" class="img-rounded" height="100" width="100">
+                        </div>
+                        <div style="padding:10px;">
+                            <?php echo "<b>".$key->residuo."</b>"; ?>
+                            <?php echo "<br>".$key->nome_cidade.", ".$key->col_uf_estado."<br>"; ?>
+                            <button type="button" class="btn btn-secondary btn-sm" data-toggle="collapse" data-target="#descricao<?php echo $i;?>">Mais Informações</button>    
+                        </div>
+                    </div>    
+                    <div id="descricao<?php echo $i;?>" class="collapse">
+                        <?php echo "Quantidade: ".$key->qtd.$key->uni_medida?><br>
+                        <div style="text-align: justify;">
+                            <?php echo $key->obs; ?>
+                        </div>
+                        <?php echo  "Período: ".date('d/m/Y',strtotime($key->data_inicio)). " à ".date('d/m/Y',strtotime($key->data_validade)); ?>
+                        <div style="color:<?php echo $key->cor; ?>;"> 
+                            <?php echo "*".$key->descricao; ?>
+                        </div>
+                   <center><button class="btn btn-primary btn-block" value="Ver" > Ver</button></center>
+                    </div>
                 </div>
-            </td>
-            <td>
-                <center><?php echo  date('d/m/Y',strtotime($key->data_inicio)). " à <br>".date('d/m/Y',strtotime($key->data_validade)); ?></center>
-            </td>
-            <td>
-                <div style="text-align: center;color:<?php echo $key->cor; ?>;"> <?php echo $key->descricao; ?></div><br>
-            <center><button class="btn btn-info" value="Ver" > Ver</button></center>
+                 <?php   }?>
             </td>
         </tr>
          <?php
@@ -52,9 +117,9 @@
         $('#lista_demandas').DataTable({                              
         "oLanguage": {
            "sProcessing": "Aguarde enquanto os dados são carregados ...",
-           "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+           "sLengthMenu": "Mostrar _MENU_ linhas de registros por pagina",
            "sZeroRecords": "Nenhum registro correspondente ao criterio encontrado",
-           "sInfoEmtpy": "Exibindo 0 a 0 de 0 registros",
+           "sInfoEmtpy": "Exibindo 0 a 0 de 0 linhas de registros",
            "sInfo": "Exibindo de _START_ a _END_ de _TOTAL_ registros",
            "sInfoFiltered": "",
            "sSearch": "Procurar",
@@ -64,7 +129,9 @@
               "sNext":     "Próximo",
               "sLast":     "Último"
            }
-        }                              
+        },  
+         "scrollX": true,
+         "lengthMenu": [[5,10, 25], [5,10, 25]]
        });
     });
     
