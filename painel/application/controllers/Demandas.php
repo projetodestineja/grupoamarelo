881440018c5448473b64d2382962273332ff70a3
Onério Sousa <onerio@tribo.ppg.br>
2017-11-08 10:49:05 -0200

    Visualizar demanda

--- painel/application/controllers/Demandas.php
+++ painel/application/controllers/Demandas.php
@@ -28,6 +28,10 @@ class Demandas extends CI_Controller {
 		$this->load->view('demandas/index',$data);
 	}
 	
+	
+	/*
+	*	Lista Demandas Ajax
+	*/
 	public function get_list(){
 		
 		$this->output->unset_template();
@@ -55,22 +59,134 @@ class Demandas extends CI_Controller {
 		$this->load->view('demandas/list',$data);
 	}
 
-
+	
+	/*
+	*	Visualizar demanda
+	*/
 	public function visualizar($id_demanda){
+		
 		$data = array();
-		$data['row'] = $this->demandas_model->get_row_demanda($id_demanda);	
+		
+		$data['menu_opcao_direita'][] = anchor(
+			'demandas/mensagem_demanda/'.$id_demanda, '<i class="fa fa-fw fa-plus-circle"></i> Enviar Mensagem', 
+			' rel="modal_add_edit" class="btn btn-primary btn-sm not-focusable"  data-toggle="tooltip" '
+		);
+		
+		$title = 'Visualizar Demanda #'.$id_demanda;
+		
+		//Title / Description / Tags
+        $this->output->set_common_meta($title, '', ''); 
+		
+		$data['menu_mapa'] = array(
+			'Demandas' => $this->uri->segment(1),
+			'Visualizar' => ''
+		);
+		
+		$this->load->model('demandas_status_model');
+		$data['result_status'] = $this->demandas_status_model->get_all();
+		
+		$data['row'] = $this->demandas_model->get_row_demanda_ver($id_demanda);	
 		$data['status_result'] = $this->demandas_model->get_result_status();
 		$this->load->view('demandas/ver',$data);
 	}
 	
+	/*
+	*	Atualiza Status da demanda
+	*/
+	public function post_update_status(){
+		
+		$this->output->unset_template();
+		$this->load->model('demandas_status_model');
 
+        $json = array();
+
+        if(!$this->input->post('status')){
+            $json['error'] = $json['error_status'] = 'Selecione o status';
+        }
+		if(!$this->input->post('id_demanda')){
+            $json['error'] = $json['error_demanda'] = 'Erro ao identificar demanda';
+        }
+
+		if (!$json) { 
+			
+			$this->demandas_model->updade_status($this->input->post('status'),$this->input->post('id_demanda'));
+			
+			$status = $this->demandas_status_model->get_row($this->input->post('status'));
+			
+			$json['ok'] = 'Status atualizado para: <b>'.$status->descricao.'</b>';
+			$json['data_update'] = date('d/m/Y H:i');
+			$json['load'] = site_url('demandas/status_demanda_historico/'.$this->input->post('id_demanda'));
+		}
+
+        echo json_encode($json);
+	}
+	
+	/*
+	*	Chamda modal demanda
+	*/
+	public function mensagem_demanda($id_demanda=0){
+		
+		$this->output->unset_template();
+		
+		$data = array();
+		
+		$data['id_demanda'] = $id_demanda;
+		$data['title'] = ($id_demanda == 0 ? 'Cadastrar Mensagem' : 'Atualizar Mensagem').': Demanda #'.$id_demanda;
+		
+		$this->load->view('demandas/form_mensagem',$data);	
+	}
+	
+	/*
+	*	Cadastra Mensagem para demanda no BD
+	*/
+	public function mensagem_demanda_post($id_demanda=0){
+		
+		$this->output->unset_template();
+
+        $json = array();
+
+		if(!$this->input->post('msg')){
+            $json['error'] = $json['error_msg'] = 'Digite a mensagem';
+        }
+		
+        if(!$this->input->post('assunto')){
+            $json['error'] = $json['error_assunto'] = 'Digite o assunto da mensagem';
+        }
+		
+		if (!$json) { 
+			/*
+			* Arqui fazemos o insert ou Upldate Há
+			*/
+			$json['close_modal'] = true;
+	    }
+
+        echo json_encode($json);	
+	}
+	
+	/*
+	*	Listar histórico de modificações de histórico
+	*/
+	public function status_demanda_historico($id_demanda=false){
+		$this->output->unset_template();
+		
+		$data = array();
+		
+		$data['result'] = $this->demandas_model->get_result_status_historico($id_demanda);
+		
+		$this->load->view('demandas/status_historico',$data);	
+		
+	}
+	
+	/*
+	*	Remover Demanda	
+	*/
 	public function deletar(){
 		$this->output->unset_template();
 		$array_itens = explode(",",$this->input->post('ids_bloquear'));
 
 		foreach($array_itens as $index => $id){
 			if (is_numeric($id) && $id >= 1){
-				$this->demandas_status_model->bloquear($id); /* model bloquear */
+				$this->demandas_status_model->bloquear($id); 
 			}
 		}
 
