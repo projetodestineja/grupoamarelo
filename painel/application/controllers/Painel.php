4b3fbbddc43aa446971a3cef70cfaa614c9707d1
Anderson <Anderson@192.168.2.102>
2017-11-08 19:48:47 -0200

    alterando endereço de chamada do JS

--- painel/application/controllers/Painel.php
+++ painel/application/controllers/Painel.php
@@ -9,14 +9,15 @@ class Painel extends CI_Controller {
 		$this->_init();
                 $this->load->model('empresa_model');
                 $this->load->model('demandas_model');
+                
+                
 	}
 
 	private function _init(){
+                
 		$this->output->set_template('default');
-
 		$this->load->js('assets/pluguins/jquery-easing/jquery.easing.min.js');
-		$this->load->js('assets/pluguins/chart.js/Chart.min.js');
-		$this->load->js('assets/pluguins/chart.js/script.js');
+
 		
 	}
 	
--- painel/application/views/painel.php
+++ painel/application/views/painel.php
@@ -1,4 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>    
+<script src="<?php echo base_url('assets/pluguins/chart.js/Chart.min.js'); ?>"></script>  
         <div class="row">
           <div class="col-xl-3 col-sm-6 mb-3">
             <div class="card text-white bg-primary o-hidden h-100">
