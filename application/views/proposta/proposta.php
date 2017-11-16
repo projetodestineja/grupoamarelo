ba67a0f8f973551e9b82effd9f786571d5b3103d
Anderson Tadeu Sgrancio <tadeusgrancio@gmail.com>
2017-11-16 16:51:57 -0200

    formulário proposta

--- application/views/proposta/proposta.php
+++ application/views/proposta/proposta.php
@@ -1,6 +1,78 @@
-    <div role="tabpanel" class="tab-pane" id="propostas">
-    	<div id="proposta" >emite proposta</div>
+<div role="tabpanel" class="tab-pane" id="propostas">
+     
+    <div class="card">
+    <h5 class="card-header"><i class="fa fa-list" ></i> Cadastro de Proposta</h5>
+    <div class="card-block">
+        <div style="padding:15px;">
+        <form id="form_proposta" name="form_proposta" action="" method="POST">
+            <div>
+                <div class="form-check form-check-inline col-md-4">    
+                    <input checked class="form-check-input" type="radio" name="cobranca" id="cobrancasim" name="cobrancasim" value="1"  onblur="atualiza_total();"><b> Cobrar para coletar o resíduo</b></input>
+                    <br>
+                    <input class="form-check-input" type="radio" name="cobranca" id="cobrancanao" name="cobrancanao" value="0"  onblur="atualiza_total();"><b> Pagar para coletar o Resíduo</b></input>
+                </div>  
+            </div>  
+            <br>
+            <div class="row">
+                <div class="form-group col-md-3 required" id="col_coleta">
+                    <label for="valor_coleta" class="col-form-label">Valor para Coleta (R$)</label>
+                    <input required type="number"  class="form-control" id="valor_coleta" name="valor_coleta" placeholder="00,00" onblur="atualiza_total();">
+                </div>
+                <div class="form-group col-md-3 required" id="col_frete">
+                    <label for="valor_frete" class="col-form-label">Valor Frete (R$)</label>
+                    <input required type="number" class="form-control" id="valor_frete" name="valor_frete" placeholder="00,00" onblur="atualiza_total();">
+                </div>
+                <div class="form-group col-md-3" id="col_total">
+                    <label for="valor_total" class="col-form-label">Total (R$)</label>
+                    <input type="number" class="form-control" id="valor_total" name="valor_total" placeholder="00,00" readonly >
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col-md-12 required" id="col_cnpj">
+                    <label for="condicoes" class="col-form-label">Condições de Pagamento</label>
+                    <input required type="text" class="form-control" id="condicoes" name="condicoes"  >
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col-md-3 required" id="col_cnpj">
+                    <label for="prazo" class="col-form-label">Prazo para Coleta (dias)</label>
+                    <input required type="number" class="form-control" id="condicoes" name="condicoes"  min="0" max="180"  >
+                </div>
+                <div class="form-group col-md-4 required" id="col_cnpj">
+                    <label for="validade" class="col-form-label">Validade da Proposta (dias)</label>
+                    <input required type="number" class="form-control" id="validade" name="validade" min="0" max="180" >
+                </div>
+            </div>
+            
+            <div >
+                <button class="btn btn-success" type="submit">Salvar</button>
+            </div>
+            
+        </form>
     </div>
-    
-    
+    </div>
+</div>
+
+
 </div><!-- NÃO APAGAR ESTE FECHAMENTO DE DIV. ELA FECHA A DIV DE DEMANDA -->
+
+<script>
+    function atualiza_total(){
+        
+        var valor_coleta = document.getElementById("valor_coleta").value;
+        var valor_frete = document.getElementById("valor_frete").value;
+        var total = 0;
+        
+        if (document.getElementById("cobrancasim").checked){
+            total = parseFloat(valor_coleta) + parseFloat(valor_frete);
+            document.getElementById("valor_total").value = total;
+        }
+        if (document.getElementById("cobrancanao").checked){
+           total = parseFloat(valor_coleta) - parseFloat(valor_frete);
+           document.getElementById("valor_total").value = total;
+        }
+        //alert('ok');
+    }
+      
+    
+</script>    
