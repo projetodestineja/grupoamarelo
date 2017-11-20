3ee2b6b452ede1566ad78a6a375c0db9ba37c4b5
Onério Sousa <onerio@tribo.ppg.br>
2017-11-20 17:16:43 -0200

    Visualizador de mensagem

--- painel/application/controllers/Mensagens.php
+++ painel/application/controllers/Mensagens.php
@@ -2,18 +2,102 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mensagens extends CI_Controller {
-
+	
+	var $table = 'mensagens';
+    var $column_order = array('id', 'assunto', 'cadastrada', 'alert_email'); //Campos da tabela para ordernar coluna
+    var $column_search = array('id', 'assunto', 'cadastrada', 'alert_email'); //campos da tabela para fazer pesquisa no banco
+    var $order = array('id' => 'desc'); // Ordem padrão do datagrid
+	
 	function __construct(){
     parent::__construct();
-    $this->login_model->restrito();
+    	$this->login_model->restrito();
 		$this->load->model('mensagens_model');
 		$this->load->helper('text');
 		$this->_init();
 	}
 
-	private function _init(){
-		$this->output->set_template('default');
-	}
+	private function _init() {
+
+        $this->output->set_template('default');
+
+        $this->load->js('assets/pluguins/datatables/datatables.min.js');
+        $this->load->js('assets/pluguins/datatables/dataTables.bootstrap4.js');
+        $this->load->js('assets/pluguins/datatables/script.js');
+    }
+
+    public function index() {
+        $this->load->helper('datatables');
+		
+		//Title / Description / Tags
+        $this->output->set_common_meta('Mensagens Enviadas', '', ''); 
+
+        $data['menu_mapa'] = array(
+            'Mensagens' => ''
+        );
+
+        $data['menu_opcao_direita'][] = anchor(
+			'#', 
+			'<i class="fa fa-fw fa-trash"></i> Remover', 
+			'class="btn btn-danger btn-sm not-focusable" id="deletar_row_table"'
+		);
+
+        //Nome Coluna / Width Coluna em PX 
+        $table_th = array(
+            array('col_name' => '<input name="select_all" value="1" id="example-select-all" type="checkbox" />', 'col_width' => 18, 'col_order' => false),
+            array('col_name' => 'Assunto', 'col_width' => NULL, 'col_order' => true),
+            array('col_name' => 'Cadastrada', 'col_width' => NULL, 'col_order' => true),
+            array('col_name' => 'E-mail', 'col_width' => NULL, 'col_order' => true),
+            array('col_name' => '--', 'col_width' => 18, 'col_order' => false)
+        );
+        $datagrid = array(
+            'grid_id' => 'table', // ID tabela html carregamento
+            'load_ajax' => site_url("mensagens/ajax_list"), // URL carregamento ajax Json
+            'delete_ajax' => site_url("mensagens/deletar"), // URL deletar registro
+            'columns' => $table_th
+        );
+
+        $data['table_th'] = $table_th;
+        $data['datagrid_js'] = datagrid_js($datagrid);
+
+        $this->load->view('theme/listar', $data);
+    }
+	
+	
+	/*
+	*	Listagem de registro ajax
+	*/
+	public function ajax_list() {
+
+        $this->output->unset_template();
+
+        $list = $this->mensagens_model->get_datatables();
+        $data = array();
+        $no = $_POST['start'];
+		
+        foreach ($list as $dados) {
+            $no++;
+            $row = array();
+            $row[] = '<input type="checkbox" value="' . $dados->id . '" rel="' . $dados->assunto . '" >';
+            $row[] = $dados->assunto;
+            $row[] = date('d/m/y H:i',strtotime($dados->cadastrada)).'h';
+            $row[] = ($dados->alert_email == true ? 'SIM' : 'NÂO');
+            $row[] = '
+			<a href="'.site_url('mensagens/visualizar/'.$dados->id).'"  class="btn btn-sm btn-warning" >
+			<i class="fa fa-fw fa-pencil-square-o"></i>
+			</a>';
+
+            $data[] = $row;
+        }
+
+        $output = array(
+            "draw" => $_POST['draw'],
+            "recordsTotal" => $this->mensagens_model->count_all(),
+            "recordsFiltered" => $this->mensagens_model->count_filtered(),
+            "data" => $data,
+        );
+
+        echo json_encode($output);
+    }
 
 
 	/*
@@ -95,7 +179,44 @@ class Mensagens extends CI_Controller {
         echo json_encode($json);	
 	}
 
-
+	
+	/*
+	*	Visualiar mensagem
+	*/
+	public function visualizar($id_mensagem=NULL){
+		
+		$data = array();
+		
+		$this->output->set_common_meta('Visualizar mensagem', '', ''); //Title / Description / Tags
+		
+		$row = $this->mensagens_model->get_row_mensagem($id_mensagem);
+		
+		if(!$row){
+			$this->session->set_flashdata('resposta_erro', 'Mensagem não identificada');
+			redirect(site_url('mensagens'));	
+		}
+		
+		$data['assunto'] = $row->assunto;
+		$data['msg'] = $row->msg;
+		$data['alert_email'] = ($row->alert_email==true?'SIM':'NÂO');
+		$data['cadastrada'] = date('d/m/y H:i',strtotime($row->cadastrada)).'h';
+		$data['atualizada'] = date('d/m/y H:i',strtotime($row->atualizada)).'h';
+		
+		
+		$data['menu_mapa'] = array(
+            'Mensagens' => $this->uri->segment(1),
+            'Visualizar' => ''
+        );
+		
+        $data['menu_opcao_direita'][] = anchor(
+			'mensagens', 
+			'<i class="fa fa-fw fa-undo"></i> Voltar', 
+			'class="btn btn-info btn-sm not-focusable"'
+		);
+		
+		$this->load->view('mensagens/visualizar_mensagem_demanda',$data);	
+	}
+	
 	/*
 	*	Listar Mensagens demanda id X
 	*/
@@ -111,7 +232,7 @@ class Mensagens extends CI_Controller {
 
 
 	/*
-	*	Remover mensagem
+	*	Remover mensagem forma simples
 	*/
 	function remover(){
 
@@ -127,6 +248,22 @@ class Mensagens extends CI_Controller {
 		}
 		echo json_encode($json);	
 	}
+	
+	/*
+	*	Remover mensagem via listagem
+	*/
+	function deletar() {
+        $this->output->unset_template();
+        $array_itens = explode(",", $this->input->post('ids_delete'));
+        foreach ($array_itens as $index => $id) {
+            if (is_numeric($id) && $id >= 1) {
+                $this->mensagens_model->deletar($id);
+            }
+        }
+        $error = "Selected countries (id's: " . $this->input->post('ids_delete') . ") deleted with success";
+        $this->output->set_header($this->config->item('ajax_header'));
+        $this->output->set_output($error);
+    }
 	
 	
 
--- painel/application/models/Mensagens_model.php
+++ painel/application/models/Mensagens_model.php
@@ -3,8 +3,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mensagens_model extends CI_Model {
 	
-	var $table = "mensagens";
+	 private function _get_datatables_query(){
+        $this->db->from($this->table);
+        $i = 0;
+        foreach ($this->column_search as $item){ // loop column 
+            if($_POST['search']['value']){  // if datatable send POST for search
+                if($i===0){ // first loop
+                    $this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+                    $this->db->like($item, $_POST['search']['value']);
+                }else{
+                    $this->db->or_like($item, $_POST['search']['value']);
+                }
+                if(count($this->column_search) - 1 == $i) //last loop
+                    $this->db->group_end(); //close bracket
+            }
+            $i++;
+        }
+        if(isset($_POST['order'])){  // here order processing
+            $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        }else 
+		if(isset($this->order)){
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
+    }
     
+ 
+    function get_datatables(){
+        $this->_get_datatables_query();
+        if($_POST['length'] != -1)
+        $this->db->limit($_POST['length'], $_POST['start']);
+        $this->db->where('removida',NULL);
+        $query = $this->db->get();
+        return $query->result();
+    }
+ 
+    function count_filtered(){
+        $this->_get_datatables_query();
+        $this->db->where('removida',NULL);
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+ 
+    public function count_all(){
+        $this->db->where('removida',NULL);
+        $this->db->from($this->table);
+        return $this->db->count_all_results();
+    }
+    
+	public function get_all(){
+		$this->db->where('removida',NULL);
+		return $this->db->get($this->table)->result();
+	}
+	
 
     /*
     *   Insert mesagem
@@ -117,7 +168,7 @@ class Mensagens_model extends CI_Model {
 
 
     /*
-    *  Remover Mensagem
+    *  Remover Mensagem forma simples
     */
     function remover($id_delete){
         $this->db->where('id',$id_delete);
@@ -132,5 +183,13 @@ class Mensagens_model extends CI_Model {
     function get_row_mensagem($id_mensagem){
         $this->db->where('id',(int)$id_mensagem);
         return $this->db->get($this->table)->row();
+    }
+	/*
+	*	Remover Via listagem
+	*/
+	public function deletar($id){
+		$this->db->where('id',$id);
+        $this->db->set('removida',date('Y-m-d H:i:s'));
+		$this->db->update($this->table); 
     }
 }
