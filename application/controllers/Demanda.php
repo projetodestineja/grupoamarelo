754713ced521cdd694c7540b34b02f4d36d14aa4
savifon <savifon1@gmail.com>
2017-11-21 21:33:20 -0200

    ajustando identação do código

--- application/controllers/Demanda.php
+++ application/controllers/Demanda.php
@@ -461,15 +461,15 @@ class Demanda extends CI_Controller {
 		
 		$data['hoje'] = date("Y-m-d");
     
-    if ($this->input->post('validade'))
-        $data['tab_ativa'] = 'proposta'; 
-    else
-        $data['tab_ativa'] = 'demanda';
-    
-		$data['menu_opcao_direita'][] = '
-		<a href="javascript:window.history.go(-1)" class="btn btn-info btn-sm not-focusable" >
-			<i class="fa fa-fw fa-undo"></i> Voltar
-		</a>';
+		if ($this->input->post('validade'))
+			$data['tab_ativa'] = 'proposta'; 
+		else
+			$data['tab_ativa'] = 'demanda';
+		
+			$data['menu_opcao_direita'][] = '
+			<a href="javascript:window.history.go(-1)" class="btn btn-info btn-sm not-focusable" >
+				<i class="fa fa-fw fa-undo"></i> Voltar
+			</a>';
 		
 		if($row['status']==6){
 			$data['menu_opcao_direita'][] = '
@@ -486,7 +486,7 @@ class Demanda extends CI_Controller {
 		$data['title'] = 'Demanda #'.$id_demanda;
 		
 		//Title / Description / Tags
-    $this->output->set_common_meta($data['title'], '', ''); 
+    	$this->output->set_common_meta($data['title'], '', ''); 
 		
 		
 		$data['menu_mapa'] = array(
@@ -504,40 +504,40 @@ class Demanda extends CI_Controller {
 	
 		$this->load->view('demanda/ver',$data);
  
-                if($this->session->userdata['empresa']['funcao']==2){ 
-                    $this->load->view('proposta/proposta',$data);
-                if ($this->input->post('btcancelar')){
-                     $this->proposta_model->delete($id_demanda,$this->session->userdata['empresa']['id']);
-                     $this->session->set_flashdata('msg_proposta', "Proposta cancelada com sucesso.");
-                } else{    
-                    
-                    if ($this->input->post('validade')){
-                        
-                        $dados['cobranca'] = $this->input->post('cobranca');
-                        $dados['id_empresa_coletora'] = $this->session->userdata['empresa']['id'];
-                        $dados['id_demanda'] = $id_demanda;
-                        $dados['valor'] = $this->input->post('valor_coleta');
-                        $dados['frete'] = $this->input->post('valor_frete');
-                        $dados['total'] = $this->input->post('valor_total');
-                        $dados['condicoes_pagamento'] = $this->input->post('condicoes');
-                        $dados['prazo_coleta'] = $this->input->post('prazo');
-                        $validade = str_replace("/", "-", $this->input->post('validade'));
-                        $validade =  date('Y-m-d', strtotime($validade));
-                        $dados['validade_proposta'] = $validade;
-                        $dados['observacoes'] = $this->input->post('obs');
-                        $dados['aceita'] = 'Não';
-                        
-                        $this->form_validation->set_rules('cobranca', 'cobranca', 'required');
-                        
-                        if ($this->form_validation->run() == TRUE){
-                            $this->proposta_model->salvar($dados);
-                            $this->session->set_flashdata('msg_proposta', "Proposta cadastrada com sucesso.");
-                            
-                        } else $this->session->set_flashdata('msg_proposta', "Erro ao cadastrar proposta.");
-                    }        
-                    }
-                        $data2 = $this->proposta_model->getrow($id_demanda);
-                        if (isset($data2->aceita) &&($data2->aceita=='Sim')) $this->session->set_flashdata('msg_proposta', "<b>Parabéns!</b> Esta proposta foi aceita.");
+		if($this->session->userdata['empresa']['funcao']==2){ 
+				$this->load->view('proposta/proposta',$data);
+			if ($this->input->post('btcancelar')){
+					$this->proposta_model->delete($id_demanda,$this->session->userdata['empresa']['id']);
+					$this->session->set_flashdata('msg_proposta', "Proposta cancelada com sucesso.");
+			} else{    
+				
+				if ($this->input->post('validade')){
+					
+					$dados['cobranca'] = $this->input->post('cobranca');
+					$dados['id_empresa_coletora'] = $this->session->userdata['empresa']['id'];
+					$dados['id_demanda'] = $id_demanda;
+					$dados['valor'] = $this->input->post('valor_coleta');
+					$dados['frete'] = $this->input->post('valor_frete');
+					$dados['total'] = $this->input->post('valor_total');
+					$dados['condicoes_pagamento'] = $this->input->post('condicoes');
+					$dados['prazo_coleta'] = $this->input->post('prazo');
+					$validade = str_replace("/", "-", $this->input->post('validade'));
+					$validade =  date('Y-m-d', strtotime($validade));
+					$dados['validade_proposta'] = $validade;
+					$dados['observacoes'] = $this->input->post('obs');
+					$dados['aceita'] = 'Não';
+					
+					$this->form_validation->set_rules('cobranca', 'cobranca', 'required');
+					
+					if ($this->form_validation->run() == TRUE){
+						$this->proposta_model->salvar($dados);
+						$this->session->set_flashdata('msg_proposta', "Proposta cadastrada com sucesso.");
+						
+					} else $this->session->set_flashdata('msg_proposta', "Erro ao cadastrar proposta.");
+				}
+			}
+			$data2 = $this->proposta_model->getrow($id_demanda);
+			if (isset($data2->aceita) && ($data2->aceita=='Sim')) $this->session->set_flashdata('msg_proposta', "<b>Parabéns!</b> Esta proposta foi aceita.");
 			$this->load->view('proposta/proposta',$data2);
                   
 		}else{
