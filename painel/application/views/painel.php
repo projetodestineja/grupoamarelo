03879247b5c30f98eeb972667fab896f9d0de624
Anderson Tadeu Sgrancio <tadeusgrancio@gmail.com>
2017-11-21 10:22:13 -0200

    teste 1

--- painel/application/views/painel.php
+++ painel/application/views/painel.php
@@ -1,6 +1,9 @@
 <script src="<?php echo base_url('assets/pluguins/chart.js/Chart.min.js'); ?>"></script>  
 
 
+
+
+
         <div class="row">
           <div class="col-xl-3 col-sm-6 mb-3">
             <div class="card text-white bg-primary o-hidden h-100">
