e27660a1e08857811b1b6418aba9fbb21b2da23e
savifon <savifon1@gmail.com>
2017-11-29 19:17:11 -0200

    adicionando legenda para campos de data

--- application/views/demanda/form_cad_demanda.php
+++ application/views/demanda/form_cad_demanda.php
@@ -92,9 +92,9 @@
               
             </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-9" >
         	<div class="row">
-            <div class="form-group col-md-6 required" >
+            <div class="form-group col-md-6 required" data-toggle="tooltip" title="Data início para recebimento de propostas">
 			<label for="data_inicio" class="col-form-label">Data início</label>
             <div class="input-group" >
 			<input name="data_inicio" class="form-control date" value="<?php echo $data_inicio;?>" placeholder="dd/mm/aaaa" id="input-data-inicio" >
@@ -103,8 +103,8 @@
             </label>
            </div>    
 		</div>
-		<div class="form-group col-md-6  required" style="padding-left:0" >
-			<label for="data_validade" class="col-form-label">Data de expiração</label>
+		<div class="form-group col-md-6 required" style="padding-left:0" data-toggle="tooltip" title="Data limite para recebimento de propostas">
+			<label for="data_validade" class="col-form-label">Data limite</label>
             <div class="input-group" >
 			<input name="data_validade" class="form-control date" value="<?php echo $data_validade;?>" placeholder="dd/mm/aaaa" id="input-data-validade"  >
             <label class="input-group-addon btn" for="testdate">
