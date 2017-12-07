69fa9a35228e88b9e9bd57e053a69b9f4b01356a
Anderson Tadeu Sgrancio <tadeusgrancio@gmail.com>
2017-12-07 20:58:20 -0200

    ajuste tooltip add demanda
    ajuste tooltip add demanda

--- application/views/demanda/form_cad_demanda.php
+++ application/views/demanda/form_cad_demanda.php
@@ -112,8 +112,8 @@
         </div>
         <div class="col-md-9" >
         	<div class="row">
-            <div class="form-group col-md-6 required" data-toggle="tooltip" title="Data início para recebimento de propostas">
-			<label for="data_inicio" class="col-form-label">Data início</label>
+            <div class="form-group col-md-6 required">
+                <label for="data_inicio" class="col-form-label">Data início <i class="fa fa-info-circle" data-toggle="tooltip" title="Data início para recebimento de propostas"></i></label>
             <div class="input-group" >
 			<input name="data_inicio" class="form-control date" value="<?php echo $data_inicio;?>" placeholder="dd/mm/aaaa" id="input-data-inicio" >
             <label class="input-group-addon btn" for="testdate">
@@ -121,8 +121,8 @@
             </label>
            </div>    
 		</div>
-		<div class="form-group col-md-6 required" style="padding-left:0" data-toggle="tooltip" title="Data limite para recebimento de propostas">
-			<label for="data_validade" class="col-form-label">Data limite</label>
+		<div class="form-group col-md-6 required" style="padding-left:0">
+			<label for="data_validade" class="col-form-label">Data limite <i class="fa fa-info-circle" data-toggle="tooltip" title="Data limite para recebimento de propostas"></i></label>
             <div class="input-group" >
 			<input name="data_validade" class="form-control date" value="<?php echo $data_validade;?>" placeholder="dd/mm/aaaa" id="input-data-validade"  >
             <label class="input-group-addon btn" for="testdate">
